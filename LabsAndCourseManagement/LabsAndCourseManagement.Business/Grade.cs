64f08439e4b91bbb1b931f1bc53e1c8196a5a175
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2022-11-06 18:27:05 +0200

    Laboratory, Grade, StudentGrades classes created with initial properties

---
+++ LabsAndCourseManagement/LabsAndCourseManagement.Business/Grade.cs
@@ -0,0 +1,11 @@
+﻿namespace LabsAndCourseManagement.Business
+{
+    public class Grade
+    {
+        public Guid Id { get; private set; }
+        public DateTime GradingDate { get; private set; }
+        public Double Mark { get; private set; }
+        public ExaminationType GradeType { get; private set; }
+        public String Mentions { get; private set; }
+    }
+}
