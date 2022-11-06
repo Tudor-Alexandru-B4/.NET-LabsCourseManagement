25adf1442fc09c86caca6f742732488fb33c7b14
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2022-11-06 20:21:41 +0200

    merging

---
+++ LabsAndCourseManagement/LabsAndCourseManagement.Business/Catalog.cs
@@ -0,0 +1,30 @@
+﻿namespace LabsAndCourseManagement.Business
+{
+    public class Catalog
+    {
+        public Guid Id { get; private set; }
+        public List<StudentGrades> StudentGrades { get; private set; }
+    
+        public static Result<Catalog> Create()
+        {
+            var catalog = new Catalog
+            {
+                Id = Guid.NewGuid(),
+                StudentGrades = new List<StudentGrades>()
+            };
+            return Result<Catalog>.Success(catalog);
+        }
+
+        public Result AddStudentGradesToCatalog(StudentGrades studentGrades)
+        {
+            if(studentGrades == null)
+            {
+                return Result.Failure("StudentGrades cannot be null");
+            }
+
+            StudentGrades.Add(studentGrades);
+            return Result.Success();
+        }
+    
+    }
+}
