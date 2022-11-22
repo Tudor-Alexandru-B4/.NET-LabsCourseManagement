082dc2afe7d18826c752878e53cb54ea10092941
Paula Malina <pastraguspaula@gmail.com>
2022-11-22 22:41:09 +0200

    added grading info-create-repo-interface

---
+++ LabsCourseManagement/LabsCourseManagement.Application/Repositories/IGradingInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using LabsCourseManagement.Domain;
+
+namespace LabsCourseManagement.Application.Repositories
+{
+    public interface IGradingInfoRepository
+    {
+        void Add(GradingInfo gradingInfo);
+        void Delete(GradingInfo gradingInfo);
+        GradingInfo Get(Guid id);
+        List<GradingInfo> GetAll();
+        void Save();
+    }
+}
--- LabsCourseManagement/LabsCourseManagement.Domain/GradingInfo.cs
+++ LabsCourseManagement/LabsCourseManagement.Domain/GradingInfo.cs
@@ -1,4 +1,7 @@
-﻿namespace LabsCourseManagement.Domain
+﻿using LabsCourseManagement.Domain.Helpers;
+using System.Xml.Linq;
+
+namespace LabsCourseManagement.Domain
 {
     public class GradingInfo
     {
@@ -9,5 +12,23 @@
         public double MaxGrade { get; private set; }
         public string Description { get; private set; }
         public TimeAndPlace TimeAndPlace { get; private set; }
+        public static Result<GradingInfo> Create(ExaminationType type, double minGrade, double maxGrade, string Description)
+        {
+            if ( minGrade <= 0 || maxGrade <= 0 || Description==null)
+            {
+                return Result<GradingInfo>.Failure("Min grade / max grade cannot be under 0 or description cannot be null");
+            }
+            var gradingInfo = new GradingInfo
+            {
+                ExaminationType = type,
+                MinGrade = minGrade,
+                MaxGrade = maxGrade,
+                Description = Description,
+                IsMandatory = true,
+                TimeAndPlace = null,
+            };
+            return Result<GradingInfo>.Success(gradingInfo);
+
+        }
     }
 }
