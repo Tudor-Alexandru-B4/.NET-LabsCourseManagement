ded33620807811a99e614ca9a4dd32a13513fa54
Paula Malina <pastraguspaula@gmail.com>
2022-11-27 20:42:10 +0200

    noi modificari

--- LabsCourseManagement/LabsCourseManagement.WebUI/Controllers/ProfessorsController.cs
+++ LabsCourseManagement/LabsCourseManagement.WebUI/Controllers/ProfessorsController.cs
@@ -10,10 +10,14 @@ namespace LabsCourseManagement.WebUI.Controllers
     public class ProfessorsController : ControllerBase
     {
         private readonly IProfessorRepository professorRepository;
+        private readonly ICourseRepository courseRepository;
+        private readonly ILaboratoryRepository laboratoryRepository;
 
-        public ProfessorsController(IProfessorRepository professorRepository)
+        public ProfessorsController(IProfessorRepository professorRepository, ICourseRepository courseRepository, ILaboratoryRepository laboratoryRepository)
         {
             this.professorRepository = professorRepository;
+            this.courseRepository = courseRepository;
+            this.laboratoryRepository = laboratoryRepository;
         }
         [HttpGet]
         public IActionResult Get()
@@ -56,5 +60,62 @@ namespace LabsCourseManagement.WebUI.Controllers
             professorRepository.Save();
             return NoContent();
         }
+        [HttpPost("{professorId:guid}/courses")]
+        public ActionResult UpdateCourses(Guid professorId,[FromBody] List<Guid> coursesId)
+        {
+            var courses = new List<Course>();
+            var professor = professorRepository.GetById(professorId);
+            var professors = new List<Professor>();
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+            foreach (var courseId in coursesId)
+            {
+                var course = courseRepository.Get(courseId);
+                if (course == null)
+                {
+                    return NotFound();
+                }
+                courses.Add(course);
+                professors.Add(professor);
+                course.AddProfessors(professors);
+            }
+            professor.AddCourses(courses);
+            professorRepository.Save();
+            courseRepository.Save();
+            return NoContent();
+
+        }
+        [HttpPost("{professorId:guid}/laboratories")]
+        public ActionResult UpdateLaboratories(Guid professorId, [FromBody] List<Guid> laboratoriesId)
+        {
+            var laboratories = new List<Laboratory>();
+            var professor = professorRepository.GetById(professorId);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+            foreach (var laboratoryId in laboratoriesId)
+            {
+                var laboratory = laboratoryRepository.Get(laboratoryId);
+                if(laboratory== null)
+                {
+                    return NotFound();
+                }
+                laboratories.Add(laboratory);
+
+            }
+            professor.AddLaboratories(laboratories);
+            professorRepository.Save();
+            return NoContent();
+
+        }
+
     }
 }
+
+
+//6813d89b-5055-4694-8beb-ef0ff63a895b curs
+//
