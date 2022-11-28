8cab03494ec53cd9c2f77036334d1ea03b646d66
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2022-11-28 15:33:35 +0200

    Routing bug, page code is done

--- LabsCourseManagement/LabsCourseManage.UI/Program.cs
+++ LabsCourseManagement/LabsCourseManage.UI/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddHttpClient<IStudentDataService, StudentDataService>
         = new Uri(builder.HostEnvironment.BaseAddress)
     );
 
+builder.Services.AddHttpClient<ILaboratoryDataService, LaboratoryDataService>
+    (
+        client => client.BaseAddress
+        = new Uri(builder.HostEnvironment.BaseAddress)
+    );
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
