f27409f3534a2debcd36f536e32780121dba0c81
Paula Malina <pastraguspaula@gmail.com>
2022-11-28 14:43:39 +0200

    prof screen

---
+++ LabsCourseManagement/LabsCourseManage.UI/Pages/Services/IProfDataService.cs
@@ -0,0 +1,13 @@
+﻿using LabsCourseManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LabsCourseManage.UI.Pages.Services
+{
+    public interface IProfDataService
+    {
+        Task<IEnumerable<Professor>> GetAllProfessors();
+        Task<Professor> GetProfessorDetail(Guid professorId);
+    }
+}
---
+++ LabsCourseManagement/LabsCourseManage.UI/Pages/Services/ProfDataService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System;
+using LabsCourseManagement.Domain;
+
+namespace LabsCourseManage.UI.Pages.Services
+{
+    public class ProfDataService : IProfDataService
+    {
+        private const string apiUrl = "http://localhost:5081/v1/api/professors";
+        private readonly HttpClient httpClient;
+
+        public ProfDataService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<IEnumerable<Professor>> GetAllProfessors()
+        {
+            return await JsonSerializer
+                .DeserializeAsync<IEnumerable<Professor>>
+                (await httpClient.GetStreamAsync(apiUrl),
+                new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true,
+                });
+        }
+
+        public async Task<Professor> GetProfessorDetail(Guid professorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
