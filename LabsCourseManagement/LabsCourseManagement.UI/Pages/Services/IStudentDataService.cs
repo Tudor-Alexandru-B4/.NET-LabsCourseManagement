b0b9c68818ffa6e90234c341f2acff2799ef7345
CotiugaIonela <cotiugaionela23@gmail.com>
2022-12-05 15:15:15 +0200

    update student-screen

--- LabsCourseManagement/LabsCourseManagement.UI/Pages/Services/IStudentDataService.cs
+++ LabsCourseManagement/LabsCourseManagement.UI/Pages/Services/IStudentDataService.cs
@@ -4,7 +4,10 @@ namespace LabsCourseManagement.UI.Pages.Services
 {
     public interface IStudentDataService
     {
+        Task CreateStudent(StudentCreateModel student);
+        Task DeleteStudent(Guid studentId);
         Task<IEnumerable<StudentModel>> GetAllStudent();
         Task<StudentModel> GetStudentDetail(Guid studentId);
+        Task UpdateStudentGroup(Guid studentId, string groupName);
     }
 }
