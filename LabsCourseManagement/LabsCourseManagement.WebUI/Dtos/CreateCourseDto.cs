36bef3df82db9ca4efac34f61c3bec4685b5b897
CotiugaIonela <cotiugaionela23@gmail.com>
2022-12-11 21:26:58 +0200

    fluent validation

--- LabsCourseManagement/LabsCourseManagement.WebUI/Dtos/CreateCourseDto.cs
+++ LabsCourseManagement/LabsCourseManagement.WebUI/Dtos/CreateCourseDto.cs
@@ -1,8 +1,19 @@
-﻿namespace LabsCourseManagement.WebUI.Dtos
+﻿using FluentValidation;
+
+namespace LabsCourseManagement.WebUI.Dtos
 {
     public class CreateCourseDto
     {
         public string Name { get; set; }
         public Guid ProfessorId { get; set; }
+    }
+
+    public class CourseValidator : AbstractValidator<CreateCourseDto>
+    {
+        public CourseValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Please insert a name");
+            RuleFor(x => x.ProfessorId).NotNull().WithMessage("Please insert a valid professor id");
+        }
     }
 }
--- LabsCourseManagement/LabsCourseManagement.WebUI/Dtos/CreateProfessorDto.cs
+++ LabsCourseManagement/LabsCourseManagement.WebUI/Dtos/CreateProfessorDto.cs
@@ -1,11 +1,23 @@
-﻿using LabsCourseManagement.Domain;
+﻿using FluentValidation;
+using LabsCourseManagement.Domain;
 
 namespace LabsCourseManagement.WebUI.Dtos
 {
     public class CreateProfessorDto
     {
-        public string Name { get;  set; }
-        public string Surname { get;  set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
         public string PhoneNumber { get; set; }
+    }
+
+    public class ProfessorValidator : AbstractValidator<CreateProfessorDto>
+    {
+        public ProfessorValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Please insert a name");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Please insert a surname");
+            RuleFor(x => x.PhoneNumber).NotEmpty().Matches(@"^(\+\d{1,3}( )?)?((\(\d{3}\))|\d{3})[- .]?\d{3}[- .]?\d{4}$")
+               .WithMessage("Numbers with this format are required: 0742844992 +40 (074)6543210, +995 (987)-654-3210");
+        }
     }
 }
