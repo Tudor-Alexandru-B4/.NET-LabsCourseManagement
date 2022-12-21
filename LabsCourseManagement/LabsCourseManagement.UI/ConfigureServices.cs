a19fb62c6c8b61f42e713693576a1b17d2169714
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2022-12-21 20:42:27 +0200

    DI rework done

---
+++ LabsCourseManagement/LabsCourseManagement.UI/ConfigureServices.cs
@@ -0,0 +1,37 @@
+﻿using LabsCourseManagement.UI.Pages.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace LabsCourseManagement.UI
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddUIServices(this IServiceCollection services, IWebAssemblyHostEnvironment env)
+        {
+            services.AddHttpClient<IProfDataService, ProfDataService>
+            (
+                client => client.BaseAddress
+                = new Uri(env.BaseAddress)
+            );
+
+            services.AddHttpClient<ICourseDataService, CourseDataService>
+                (
+                    client => client.BaseAddress
+                    = new Uri(env.BaseAddress)
+                );
+
+            services.AddHttpClient<IStudentDataService, StudentDataService>
+                (
+                    client => client.BaseAddress
+                    = new Uri(env.BaseAddress)
+                );
+
+            services.AddHttpClient<ILaboratoryDataService, LaboratoryDataService>
+                (
+                    client => client.BaseAddress
+                    = new Uri(env.BaseAddress)
+                );
+
+            return services;
+        }
+    }
+}
