3f6d908ed8c60463f6b00885a64c20466c76e355
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2023-01-09 16:38:16 +0200

    last push Yey

--- LabsCourseManagement/LabsCourseManagement.IntegrationTests/CoursesControllerTests.cs
+++ LabsCourseManagement/LabsCourseManagement.IntegrationTests/CoursesControllerTests.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 using LabsCourseManagement.Domain;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace LabsCourseManagement.IntegrationTests
 {
